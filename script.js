34fd551fea5273ebd9697bae41f1682524b121e1
Colton Trainor <trainorcolton@gmail.com>
2025-02-09 07:38:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+//PART 1...
+// Declare two variables
+let num1 = 20;
+let num2 = 10;
+
+// Perform arithmetic operations
+console.log("Addition:", num1 + num2);
+console.log("Subtraction:", num1 - num2);
+console.log("Multiplication:", num1 * num2);
+console.log("Division:", num1 / num2);
+console.log("Modulus:", num1 % num2);
+
+//PART 2...
+// Compare num1 and num2 using relational operators
+console.log("num1 > num2:", num1 > num2);
+console.log("num1 < num2:", num1 < num2);
+console.log("num1 >= num2:", num1 >= num2);
+console.log("num1 <= num2:", num1 <= num2);
+console.log("num1 === num2:", num1 === num2);
+console.log("num1 !== num2:", num1 !== num2);
+
+//PART 3...
+// Declare boolean variables
+let isSunny = false;
+let isWeekend = true;
+
+// Use logical operators
+console.log("isSunny AND isWeekend:", isSunny && isWeekend);
+console.log("isSunny OR isWeekend:", isSunny || isWeekend);
+
+//PART 4...
+// Declare and modify a variable
+let score = 100; // Sets inital score to 100
+console.log("Score:", score);
+
+score += 12; // Adds 12
+console.log("After += 12:", score);
+
+score -= 7; // Subtracts 7
+console.log("After -= 7:", score);
+
+score *= 4; // Multiplies by 4
+console.log("After *= 4:", score);
+
+//PART 5...
+// Capture user input
+let userName = prompt("Enter your Name:");
+let userAge = prompt("Enter your Age:");
+
+// Display user input in the console
+console.log("User Name:", userName);
+console.log("User Age:", userAge);
